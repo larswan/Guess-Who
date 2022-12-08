9814a238c7b8cfbe8f78a2f6a6fe67032ebed896
Larson Collier <collierlarson@gmail.com>
2022-12-07 21:26:24 -0500

    last commit pre merge

--- guessWho/src/p2Response.jsx
+++
@@ -1,9 +0,0 @@
-function p2Response({ p2r, setp2r, oldP1Q }) {
-    return (
-        <div>
-            <h3>{oldP1Q}</h3>
-            <input id="p2Response" className="input" onChange={(e) => { setp2r(e.target.value); console.log(p2r) }} type="text" placeholder="Yes or No" value={p2r} /> <br />
-        </div>
-    )
-}
-export default p2Response
